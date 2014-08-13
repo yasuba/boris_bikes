ac22d76b7429088c9903c2bf033fe7c379f63e0d
Henry Stanley <henry@henrystanley.com>
2014-08-13 17:04:07 +0100

    Added failing 'take bike' test for Person

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,8 +1,16 @@
 require 'person'
 
 describe Person do
+
 	it 'does not have a bike when initialised' do
 		person = Person.new
 		expect(person.has_bike?).to eq false
+	end
+
+	it 'can have a bike' do
+		person = Person.new
+		bike = Bike.new
+		person.take(bike)
+		expect(person.has_bike?).to eq true
 	end
 end
