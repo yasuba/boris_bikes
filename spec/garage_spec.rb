6cb6f02b09ab4d569f39953b6477739c19486be3
Henry Stanley <henry@henrystanley.com>
2014-08-13 11:27:43 +0100

    Added an error for Maya

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -9,4 +9,17 @@ describe Garage do
 		expect(garage.accept(working_bike)).to eq([working_bike])
 	end
 
+	it 'should fix broken bikes when it accepts them' do
+		garage = Garage.new
+		broken_bike = Bike.new
+		broken_bike.break!
+		garage.accept(broken_bike)
+		expect(broken_bike).to be_working
+	end
+
+	it 'should not accept working bikes' do
+		garage = Garage.new
+		working_bike = Bike.new
+		expect{ garage.accept(working_bike) }.to raise_error
+	end
 end
